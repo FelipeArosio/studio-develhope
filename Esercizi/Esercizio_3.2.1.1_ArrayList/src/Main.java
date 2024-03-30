683e34cad7b5b940bcbed3f26c12bbd5f041d834
Felipe Arosio <felipe_arosio@outlook.com>
2024-03-30 15:04:14 +0100

    Ho finito la lazione di pratica Exceptions

--- Esercizi/Esercizio_3.2.1.1_ArrayList/src/Main.java
+++ Esercizi/Esercizio_3.2.1.1_ArrayList/src/Main.java
@@ -1,7 +1,29 @@
+/*
+* Creare una classe Student che accenti nel costruttore il parametro name (String) e age (Int)
+* In questa classe avere i metodi pubblici per ottenere le informazioni.
+* Creare un ArrayList con n elementi e stamparlo in console.
+* Aggiungere 4 elementi alla collezione e stampare la collezione aggiornata in console */
+
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
 
+        ArrayList<Student> listStudents = new ArrayList<>();
 
-        System.out.println("Hello world!");
+        listStudents.add(new Student("Felipe",27));
+        listStudents.add(new Student("Gabriel", 27));
+        listStudents.add(new Student("Miguel", 1));
+        listStudents.add(new Student("Artur", 3));
+
+
+        System.out.println("Lista completa: ");
+        printStudents(listStudents);
+
+    }
+    public static void printStudents (ArrayList<Student> listStudents){
+        for(Student student: listStudents) {
+            System.out.println("nome: " + student.getName() + ", età: " + student.getAge());
+        }
     }
 }
