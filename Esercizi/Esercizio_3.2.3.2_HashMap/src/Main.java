035cb6c3c65a2125aa607e8fe7daf1f13a7e1ed7
Felipe Arosio <felipe_arosio@outlook.com>
2024-03-31 17:54:57 +0200

    Ho finito la lazione di pratica Exceptions

---
+++ Esercizi/Esercizio_3.2.3.2_HashMap/src/Main.java
@@ -0,0 +1,13 @@
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args) {
+
+        HashMap<String, Integer> firstMap = new HashMap<>();
+
+
+    }
+}
