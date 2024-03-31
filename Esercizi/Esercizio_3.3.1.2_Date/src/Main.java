73edf9da7cd8c02215311975d31678dd9fddca2f
Felipe Arosio <felipe_arosio@outlook.com>
2024-03-31 21:33:28 +0200

    Ho finito la lazione di pratica Exceptions

--- Esercizi/Esercizio_3.3.1.2_Date/src/Main.java
+++ Esercizi/Esercizio_3.3.1.2_Date/src/Main.java
@@ -3,10 +3,15 @@
 * Formatta la data ottenendo 01 marzo 2023
 * Stampa sulla console   */
 
+import java.time.OffsetDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+
 public class Main {
     public static void main(String[] args) {
+        OffsetDateTime data = OffsetDateTime.parse("2023-03-01T13:00:00Z");
 
-
-        System.out.println("Hello world!");
+        String dataIta = data.format(DateTimeFormatter.ofPattern("dd MMMM yyyy", Locale.ITALY));
+        System.out.println("Data: " + dataIta);
     }
 }
