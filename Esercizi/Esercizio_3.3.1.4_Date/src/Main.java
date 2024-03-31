664fea966b59ac85d0e2f1d9b1e5c752ea3bfcd4
Felipe Arosio <felipe_arosio@outlook.com>
2024-03-31 22:08:28 +0200

    Ho finito la lazione di pratica Exceptions

--- Esercizi/Esercizio_3.3.1.4_Date/src/Main.java
+++ Esercizi/Esercizio_3.3.1.4_Date/src/Main.java
@@ -1,7 +1,26 @@
+/*
+* Crea un oggetto data da questa stringa 2023-03-01T13:00:00Z
+* aggiungi un anno
+* sottrai un mese
+* aggiungi 7 giorni
+* Stampa il risultato localizzata per l'Italia   */
+
+import java.time.OffsetDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+
 public class Main {
     public static void main(String[] args) {
 
+        OffsetDateTime data = OffsetDateTime.parse("2023-03-01T13:00:00Z");
 
-        System.out.println("Hello world!");
+        String piuAnnoData = String.valueOf(data.plusYears(1).format(DateTimeFormatter.ofPattern("yyyy", Locale.ITALY)));
+        System.out.println("Nuovo Anno: " + piuAnnoData);
+
+        String menoMeseData = String.valueOf(data.minusMonths(1).format(DateTimeFormatter.ofPattern("MMMM", Locale.ITALY)));
+        System.out.println("Nuovo Mese: " + menoMeseData);
+
+        String piuGiorniData = String.valueOf(data.plusDays(7).format(DateTimeFormatter.ofPattern("dd", Locale.ITALY)));
+        System.out.println("Nuovo Giorno: " + piuGiorniData);
     }
 }
