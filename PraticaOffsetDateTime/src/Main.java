a2c0d74ba5b41f4072b92ab94ec9e329cbcdc65b
Felipe Arosio <felipe_arosio@outlook.com>
2024-03-31 20:13:10 +0200

    Ho finito la lazione di pratica Exceptions

--- PraticaOffsetDateTime/src/Main.java
+++ PraticaOffsetDateTime/src/Main.java
@@ -1,5 +1,21 @@
+import java.time.OffsetDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
+import java.util.Locale;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        OffsetDateTime adesso = OffsetDateTime.now();
+        System.out.println("adesso " + adesso);
+
+        OffsetDateTime oggi = adesso.plusDays(1).minusHours(1);
+
+        String dateStringAdesso = adesso.format(DateTimeFormatter.ofLocalizedDateTime(FormatStyle.MEDIUM));
+        System.out.println("dateString: " + dateStringAdesso);
+
+        String dateStringOggi = oggi.format(DateTimeFormatter.ofLocalizedDateTime(FormatStyle.MEDIUM));
+        System.out.println("dateString: " + dateStringOggi);
+
     }
 }
