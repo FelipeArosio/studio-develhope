727ce747fcba9406adaa5d0926f5473603edf641
Felipe Arosio <felipe_arosio@outlook.com>
2024-03-31 21:24:20 +0200

    Ho finito la lazione di pratica Exceptions

--- Esercizi/Esercizio_3.3.1.1_Date/src/Main.java
+++ Esercizi/Esercizio_3.3.1.1_Date/src/Main.java
@@ -1,5 +1,26 @@
+/*
+* Crea un oggetto OffsetDateTime da questa stringa 2002-03-01T13:00:00Z
+* Formatta la data ottenuta in FULL, MEDIUM e SHORT
+* Stampa le varie versioni  */
+
+import java.time.OffsetDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        ZonedDateTime stringDate = OffsetDateTime.parse("2002-03-01T13:00:00Z").toZonedDateTime();
+
+        String stringDateLong = stringDate.format(DateTimeFormatter.ofLocalizedDateTime(FormatStyle.LONG));
+        System.out.println("Data nel formato 'Long': " + stringDateLong);
+
+        String stringDateMedium = stringDate.format(DateTimeFormatter.ofLocalizedDateTime(FormatStyle.MEDIUM));
+        System.out.println("Data nel formato 'Medium': " + stringDateMedium);
+
+        String stringDateShort = stringDate.format(DateTimeFormatter.ofLocalizedDateTime(FormatStyle.SHORT));
+        System.out.println("Data nel formato 'Short': " + stringDateShort);
     }
 }
