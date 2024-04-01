33fdf29e389f689a333b999280236cf80a70a5fe
Felipe Arosio <felipe_arosio@outlook.com>
2024-04-01 12:04:51 +0200

    Ho finito la lazione di pratica Exceptions

--- Esercizi/Esercizio_3.3.1.5_Date/src/Main.java
+++ Esercizi/Esercizio_3.3.1.5_Date/src/Main.java
@@ -1,5 +1,25 @@
+/*
+* Crea un oggetto data da questa stringa 2023-03-01T13:00:00Z
+* Crea un secondo oggetto data da questa stringa 2024-03-01T13:00:00Z
+* Verificare che la prima data è precedente alla seconda
+* Verificare che la seconda data è successiva alla prima
+* Verificare che le due date sono uguali ad ora
+* Stampa il risultato     */
+
+import java.time.LocalDateTime;
+import java.time.OffsetDateTime;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+
+        OffsetDateTime data1 = OffsetDateTime.parse("2023-03-01T13:00:00Z");
+        OffsetDateTime data2 = OffsetDateTime.parse("2024-03-01T13:00:00Z");
+
+        boolean controloDataPre = data1.isBefore(data2);
+        System.out.println("La prima data è precedente alla seconda: " + controloDataPre);
+
+        boolean controloDataSuc = data2.isAfter(data1);
+        System.out.println("La seconda data è successiva alla prima: " + controloDataSuc);
+
+
 }
