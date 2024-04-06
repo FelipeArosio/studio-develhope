f825ba4f019a612b5222847f0a1ee0f1c61bdf86
Felipe Arosio <felipe_arosio@outlook.com>
2024-04-06 15:42:08 +0200

    Ho finito la lazione di pratica Exceptions

---
+++ Esercizi/Esercizio_3.4.1.1_BigDecimal/src/Main.java
@@ -0,0 +1,10 @@
+/*
+- Scrivi un programma Java dove imposti 2 BigDecimal e 1 operazione aritmetica (Addizione, sottrazione, moltiplicazione e divisione, min e max) definita in un enum
+- Crei un metodo per calcolare l'operazione richiesta e e ritorni il risultato.
+- Il tipo di operazione, i due dati in input e il risultato dovranno essere stampati a schermo */
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
