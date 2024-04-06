9d2804840fb86881bf673848708d08f151f5e8d8
Felipe Arosio <felipe_arosio@outlook.com>
2024-04-06 19:16:50 +0200

    Ho finito la lazione di pratica Exceptions

--- PraticaOperatoreTernario/src/Main.java
+++ PraticaOperatoreTernario/src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int number = 5;
+/*         String message = "";
+
+       if (number < 5) {
+            message = "minore di 5";
+        } else {
+            message = "maggiore o uguale a 5";
+        }*/
+
+        final String message = number < 5
+                ? "minore di 5"
+                : "maggiore o uguale a 5";
+
+        System.out.println(message);
     }
 }
