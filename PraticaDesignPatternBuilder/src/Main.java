72ce6a6609c5c6f9186ad556126c24c4f26928fa
Felipe Arosio <felipe_arosio@outlook.com>
2024-04-12 14:35:23 +0200

    Ho finito la lazione di pratica Exceptions

--- PraticaDesignPatternBuilder/src/Main.java
+++ PraticaDesignPatternBuilder/src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        CarBuilder builder = new CarBuilder();
+        builder.setColore("Branco");
+        builder.setNumeroDiPorte(2);
+        builder.setPropulsione("benzina");
+        builder.setTipo("Sportiva");
+        Car macchinaMia = builder.build();
+
+
+        System.out.println("macchinaMia: " + macchinaMia);
+
+        //Car macchinaMia2 = new Car(4,"Rosso","Diesel","SUV");
+       // System.out.println("macchinaMia2: " + macchinaMia2);
     }
 }
